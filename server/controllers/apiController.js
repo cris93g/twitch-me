e39d29bd4d5f14fd4989703dd9d202f2291d3cae
cris93g <37388115+cris93g@users.noreply.github.com>
2019-02-12 16:37:28 -0600

    getting top games from api now

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -3,19 +3,33 @@ const axios = require("axios");
 
 axios
 	.get(
-		`https://api.twitch.tv/kraken/streams/yassuo?client_id=gue31zwe29qzw9mlr2xmcje34ktit2`
+		`https://api.twitch.tv/kraken/streams?client_id=gue31zwe29qzw9mlr2xmcje34ktit2&limit=2`
 	)
 	.then(response => {
 		streams = response.data;
 		console.log(streams);
 		return streams;
 	});
-
 let getStreams = (req, res) => {
 	res.status(200).send(streams);
 	console.log(streams);
+
+	axios
+		.get(
+			`https://api.twitch.tv/kraken/games/top?client_id=gue31zwe29qzw9mlr2xmcje34ktit2&limit=5`
+		)
+		.then(response => {
+			games = response.data;
+			console.log(games);
+			return games;
+		});
+};
+let getTopGames = (req, res) => {
+	res.status(200).send(games);
+	console.log(games);
 };
 
 module.exports = {
-	getStreams
+	getStreams,
+	getTopGames
 };
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,5 +1,6 @@
-const { getStreams } = require("../controllers/apiController");
+const { getStreams, getTopGames } = require("../controllers/apiController");
 
 module.exports = app => {
-	app.get("/api/streams", getStreams);
+	app.get("/api/top-streams", getStreams);
+	app.get("/api/top-games", getTopGames);
 };
