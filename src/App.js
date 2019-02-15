c09648ee7a6ca8e6719e98e2fa13307c81b761e0
cris93g <37388115+cris93g@users.noreply.github.com>
2019-02-14 18:28:49 -0600

    updated a few things

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
 import routes from "./routes";
+import "./App.css";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 class App extends Component {
-	render() {
-		return (
-			<Provider store={store}>
-				<div className="App">{routes}</div>
-			</Provider>
-		);
-	}
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">{routes}</div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
--- src/components/LeftBar/LeftBar.js
+++ src/components/LeftBar/LeftBar.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
 import "./LeftBar.css";
 class LeftBar extends Component {
-	render() {
-		return <div className="leftbar">leftbar</div>;
-	}
+  render() {
+    return (
+      <div className="leftbar">
+        <div>categories</div>
+        <div>all games</div>
+        <div>streams</div>
+        <div>my favorites</div>
+      </div>
+    );
+  }
 }
 
 export default LeftBar;
