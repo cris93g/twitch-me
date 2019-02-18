92936b584a30cfe77cd52c65c1e101a8d26c4988
cris93g <37388115+cris93g@users.noreply.github.com>
2019-02-17 18:03:42 -0600

    styling

--- src/components/Streams/Streams.js
+++ src/components/Streams/Streams.js
@@ -1,7 +1,22 @@
 import React, { Component } from "react";
-
+import "./Streams.css";
 import { connect } from "react-redux";
 import axios from "axios";
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+	width: 100%;
+
+	display: flex;
+	height: 100%;
+
+	color: white;
+`;
+
+const TextWrapper = styled.section`
+	margin: auto;
+	text-align: center;
+`;
 
 class Streams extends Component {
 	state = {
@@ -18,15 +33,19 @@ class Streams extends Component {
 		const { streams } = this.state;
 		const streamsDisplay = streams.map((stream, e) => {
 			return (
-				<div>
-					<a href={stream.channel.url}>
-						<img src={stream.preview.medium} />
-					</a>
-					<p>{stream.channel.display_name}</p>
-					<p>{stream.channel.status}</p>
-					<p>{`Game: ${stream.game}`}</p>
-					<p>{`Viewers: ${stream.viewers}`}</p>
-				</div>
+				<Wrapper>
+					<div className="cards">
+						<a href={stream.channel.url}>
+							<img className="card-picture" src={stream.preview.medium} />
+						</a>
+						<TextWrapper>
+							<p>{stream.channel.display_name}</p>
+							<p>{stream.channel.status}</p>
+						</TextWrapper>
+						{/* <p>{`Game: ${stream.game}`}</p> */}
+						{/* <p>{`Viewers: ${stream.viewers}`}</p> */}
+					</div>
+				</Wrapper>
 			);
 		});
 		return <div className="">{streamsDisplay}</div>;
--- src/content/Landing/Landing.js
+++ src/content/Landing/Landing.js
@@ -1,18 +1,42 @@
 import React, { Component } from "react";
+import "./Landing.css";
 import TopBar from "../../components/TopBar/TopBar";
 import LeftBar from "../../components/LeftBar/LeftBar";
 import Streams from "../../components/Streams/Streams";
 import Games from "../../components/Games/Games";
+import styled from "styled-components";
+
+const MiddleWrapper = styled.section`
+	/* display: flex; */
+	width: 30%;
+	height: 30%;
+	/* justify-content: space-between; */
+	margin: auto;
+`;
+
+// const Wrapper = styled.section`
+// 	display: flex;
+// 	flex-direction: row;
+// 	flex-wrap: wrap;
+// 	justify-content: space-between;
+// 	/* align-items: flex-start; */
+// 	align-content: stretch;
+// `;
+
 class Landing extends Component {
 	render() {
 		return (
-			<div className="Landing">
+			// <Wrapper>
+			<div className="landing">
 				{/* <LeftBar /> */}
-				<TopBar />
+				{/* <TopBar /> */}
 
 				{/* <Games /> */}
-				<Streams />
+				<MiddleWrapper>
+					<Streams />
+				</MiddleWrapper>
 			</div>
+			// </Wrapper>
 		);
 	}
 }
